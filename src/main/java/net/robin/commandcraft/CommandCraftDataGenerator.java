d930c3db76d5565d3ab42a9ac62fc038a158c2da
Xiaoyang Zhong <32957875+RobOHt@users.noreply.github.com>
2024-09-08 00:43:50 -0400

    initial commit

---
+++ src/main/java/net/robin/commandcraft/CommandCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.robin.commandcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CommandCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
